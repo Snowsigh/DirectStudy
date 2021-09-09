2c143dbc4fb545a8cc6ceb83631b4a2544e2374b
Sliem <korast@naver.com>
2021-09-09 18:38:07 +0900

    STL List stack vector 추가

---
+++ STLLLLL/STL.h
@@ -0,0 +1,92 @@
+#pragma once
+#include<iostream>
+#include<vector>
+#include<list>
+#include<queue>
+#include<map>
+#include<stack>
+#include<deque>
+#include<algorithm>
+
+template <typename T>
+class S_vector
+{
+public:
+	void vector_Show(T _Value);
+
+};
+template <typename T>
+void S_vector<T>::vector_Show(T _Value)
+{
+	std::vector<T> _vector;
+
+	for (int i = 0; i < 10; i++)
+	{
+		_vector.push_back(i);
+	}
+	_vector.push_back(_Value);
+	_vector.erase(_vector.begin()+3);
+	for (int i = 0; i < _vector.size(); i++)
+	{
+		std::cout << _vector[i] << " ";
+	}
+	std::cout << "\n";
+}
+
+template <typename T>
+class S_Stack
+{
+public:
+	void Stack_Show(T _Value);
+
+};
+template <typename T>
+void S_Stack<T>::Stack_Show(T _Value)
+{
+	std::stack<T> _stack;
+
+	for (int i = 0; i < 10; i++)
+	{
+		_stack.push(i+0.5);
+	}
+	_stack.pop();
+	_stack.push(_Value);
+	
+	int Max_Size = _stack.size();
+	for (int i = 0; i < Max_Size; i++)
+	{
+		std::cout << _stack.top() << " ";
+		_stack.pop();
+	}
+	std::cout << "\n";
+}
+
+template <typename T>
+class S_List
+{
+public:
+	void List_Show(T _Value);
+
+};
+template <typename T>
+void S_List<T>::List_Show(T _Value)
+{
+	std::list<T> _List;
+
+	for (int i = 0; i < 10; i++)
+	{
+		_List.push_back(i);
+	}
+	_List.pop_back();
+	_List.push_back(_Value);
+	_List.sort();
+
+	int Max_Size = _List.size();
+	for (int i = 0; i < Max_Size; i++)
+	{
+		std::cout << _List.front() << " ";
+		_List.pop_front();
+	}
+	std::cout << "\n";
+}
+
---
+++ STLLLLL/STLLLLL.cpp
@@ -0,0 +1,13 @@
+﻿#include "STL.h"
+
+
+int main()
+{
+	S_vector<int> vec1;
+	vec1.vector_Show(40);
+	S_Stack<double> Stc1;
+	Stc1.Stack_Show(10.5);
+	S_List<int> list1;
+	list1.List_Show(22);
+
+}
